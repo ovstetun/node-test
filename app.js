bee1eda31098a6cb5d2e7507c79809ed1374a02c
Trond Marius Øvstetun <trond@ovstetun.no>
2012-05-31 22:02:41 +0200

    Add buster to test and cleanup package.json. Restructure directory structure to use lib and test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./lib/routes');
 
 var app = module.exports = express.createServer();
 
---
+++ test/routes/index-test.js
@@ -0,0 +1,15 @@
+var buster = require('buster');
+
+var index = require('../../lib/routes/index');
+
+buster.testCase('index', {
+  'shoud render index' : function() {
+    var res = { render: this.stub() };
+    var req = this.stub();
+
+    index.index(req, res);
+
+    assert.calledOnceWith(res.render, 'index', {title : 'Express'});
+    refute.called(req);
+  },
+});
